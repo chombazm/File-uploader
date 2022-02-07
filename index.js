d658e80159686847bc478a78aacee0b0fb90aea4
chomba <chomba@bongohive.co.zm>
2022-02-07 20:37:54 +0200

    file uploading starting poin

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+const multer = require('multer');
+const ejs = require('ejs');
+const path = require('path');
+
+const port = process.env.PORT || 3000
+// [TODO: move this function to a separate file]
+// const renameFile = (file) => (file + '_' + Date.now() + path.extname(file));
+
+const storage = multer.diskStorage({
+    destination: './public/uploads/',
+    filename: function (req, file, cb) {
+        // const file = req.file.originalname;
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    },
+})
+
+// [TODO: add muliple file upload]
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 2
+    }
+}).single('fileUploader');
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Home page'
+    })
+})
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        if (err) {
+            res.render('index', {
+                title: 'failed to upload image',
+                msg : err + "could not upload file"
+            })
+            console.error(err)
+        }else {
+            // [TODO: upload image to database] 
+             res.render('index', {
+                title: 'File uploaded',
+                 msg : "file uploaded successfully"
+             })
+        }
+    })
+    // console.log(req.files);
+    // res.send('uploaded');
+})
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+})
